963b8929b70dd67c1a10a35ec2af3ffb2d160dbe
Felipe Vieira da Cunha Serafim <feserafim@gmail.com>
2017-03-27 13:21:01 +0200

    Update module version

--- src/model/csp/CSPModel.ts
+++ src/model/csp/CSPModel.ts
@@ -1,5 +1,5 @@
 /*!
-governify-csp-tools 0.2.0, built on: 2017-03-27
+governify-csp-tools 0.2.1, built on: 2017-03-27
 Copyright (C) 2017 ISA group
 http://www.isa.us.es/
 https://github.com/isa-group/governify-csp-tools
